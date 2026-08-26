6fe76cf52830731e77553db5139f388f0c286b23
PrimalMagikarp <charlesdugaw@gmail.com>
2026-08-26 10:39:16 -0400

    main css

--- server.js
+++ server.js
@@ -3,12 +3,23 @@ const http = require('http'),
       port = 3000
 
 const server = http.createServer( function( request,response ) {
+  // const stripped = request.url.slice(1)
+  // if(request.url != '/')
+  // {
+  //   sendFile(response, stripped)
+  // } 
+  // else{
+  //   sendFile(response, 'index.html')
+  // }
   switch( request.url ) {
     case '/':
       sendFile( response, 'index.html' )
       break
     case '/index.html':
       sendFile( response, 'index.html' )
+      break
+    case '/main.css':
+      sendFile( response, 'main.css' )
       break
     default:
       response.end( '404 Error: File Not Found' )
